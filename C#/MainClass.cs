95ff8fe05854b77b71f27f5f58f64b891d9a7227
Dharmaturtle <dharmaturtle@gmail.com>
2013-11-17 04:57:45 -0600

    Added Mod and Basic commands, and deltatimeformatting to Main

--- C#/MainClass.cs
+++ C#/MainClass.cs
@@ -1,79 +1,105 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
 using System.Text.RegularExpressions;
+
 
 namespace IRCbot{
 	class MainClass{
 		public static void Main(string[] args){
 			int i = 0;
-			string received, sender, message;
+			string received, sender, message, Message;
 			string[] mods = Constants.mods; //can't rely on +o because jtv servers can be very slow with applying modes
-			Regex parserawirc = new Regex(@":(.*)!.*(?:privmsg).*?:(.*)", RegexOptions.Compiled); // compiled for that lovely speed
+			Regex parserawirc = new Regex(@":(.*)!.*(?:privmsg).*?:(.*)", RegexOptions.IgnoreCase | RegexOptions.Compiled); // compiled for that lovely speed
 
 			connect();
-			for (received = MyGlobals.input.ReadLine(); ; received = MyGlobals.input.ReadLine()) {
+			while (true) { // TODO: Giant catch that logs all errors 'cept Keyboard Break and leaves bot running lovely
 				i++;
-				received = received.Trim(new Char[] {'\r','\n',' '});
-				received = received.ToLower(); // TODO: Combine into one when you figure out what the bug is
-				//Display received irc message
-				//Console.WriteLine(received);  // raw irc
-				//Console.WriteLine(i);
+				received = MyGlobals.input.ReadLine().Trim(new Char[] { '\r', '\n', ' ' });
 
-				if (received.Length == 0) { // Disconnected!
-					connect();
-				}
+				/* disconnected */
+				if (received.Length == 0) connect();
 
-				//Get user & message if its a privmsg
+				/* get user & privmsg */
 				Match sendermessage = parserawirc.Match(received);
-				sender = sendermessage.Groups[1].Value;
-				message = sendermessage.Groups[2].Value;
+				sender	= sendermessage.Groups[1].Value;
+				Message	= sendermessage.Groups[2].Value;
+				message	= Message.ToLower();
 				if (sendermessage.Success) {
-					Console.WriteLine(sender);
-					Console.WriteLine(message);
 
-					if (mods.Contains(sender)) {
-						ModClass.parse(message);
-					}
+					/* log their latest lines */
+					object[] stalkinfo = { DateTime.Now, Message };
+					MyGlobals.stalk[sender] = stalkinfo;
+					SerializeObject(MyGlobals.stalk, "stalk.xml"); // TODO: move this to PONG to reduce writes, just here for now to test
+
+					/* print to console */
+					if (new string[] { "dharm", "darm", "dhram" }.Any(received.Contains)) //http://stackoverflow.com/a/2912483/625919
+						log(sender + ": " + message, ConsoleColor.Green);
 					else {
-						Console.WriteLine("Plebian");
+						log(sender + ": " + message, ConsoleColor.Gray);
 					}
 
-					/*if (i == 16) {
-						sendmessage("tester");
-					}*/
+					/* mod */
+					if (mods.Contains(sender)) { // TODO: make sure you append longspamlist if over x lines
+						ModClass.parse(message);
+						BasicCommandsClass.parse(message);
+					}
+					/* pleb */
+					else {
+						BanLogicClass.parse(message);
+						if (((DateTime.Now - MyGlobals.pleblag).TotalSeconds > 15) && (MyGlobals.isbanned == false)) {
+							BasicCommandsClass.parse(message);
+						}
+					}
 				}
-				//Send pong to any pings
+				/* pongs */
 				else if (received.StartsWith("PING ")) {
 					WriteAndFlush(received.Replace("PING", "PONG") + "\n");
+					log(received, ConsoleColor.DarkGray);
+				}
+				/* print server text */
+				else {
+					log(received, ConsoleColor.DarkGray);
 				}
 			}
 		}
+
 		public static class MyGlobals{
 			public static System.Net.Sockets.TcpClient sock = new System.Net.Sockets.TcpClient();
 			public static System.IO.TextReader input;
 			public static System.IO.TextWriter output;
+			public static DateTime pleblag;
+			public static Boolean isbanned = false;
+			public static int									ninja		= DeSerializeObject<int>("ninja.xml"),
+																modabuse	= DeSerializeObject<int>("modabuse.xml"),
+																bancount	= DeSerializeObject<int>("bancount.xml");
+			public static List<string>							banwords	= DeSerializeObject<List<string>>("banwords.xml"),
+																tempbanwords= DeSerializeObject<List<string>>("tempbanwords.xml");
+			public static SerializableDictionary<string, object[]> stalk	= DeSerializeObject<SerializableDictionary<string, object[]>>("stalk.xml");
 		}
+
 		public static void sendmessage(string msg){
 			WriteAndFlush("PRIVMSG " + Constants.chan + " :" + msg + "\n");
 		}
+
+		/* connect to server */
 		public static void connect() {
 			int port = 6667;
-			string	nick = Constants.nick,
-					owner = Constants.owner,
+			string	nick   = Constants.nick,
+					owner  = Constants.owner,
 					server = Constants.server,
-					chan = Constants.chan,
-					oauth = Constants.oauth;
-
-			//Connect to server
+					chan   = Constants.chan,
+					oauth  = Constants.oauth;
 			MyGlobals.sock.Connect(server, port);
 			if (!MyGlobals.sock.Connected) {
-				Console.WriteLine("Failed to connect!");
+				log("Failed to connect!", ConsoleColor.Red);
 				return;
 			}
 			MyGlobals.input = new System.IO.StreamReader(MyGlobals.sock.GetStream());
 			MyGlobals.output = new System.IO.StreamWriter(MyGlobals.sock.GetStream());
-
-			//Login & join
 			WriteAndFlush(
 				"PASS " + oauth + "\n" +
 				"USER " + nick + " 0 * :" + owner + "\n" +
@@ -81,9 +107,88 @@ namespace IRCbot{
 				"JOIN " + chan + "\n" // could wait for 001 from server, but this joins anyway
 			);
 		}
+		
+		/* Why write two lines when you can write one */
 		public static void WriteAndFlush(string str) {
 			MyGlobals.output.Write(str);
 			MyGlobals.output.Flush();
+		}
+		
+		/* colors console and prepends timestamp */
+		public static void log(string text, ConsoleColor color = ConsoleColor.White) {
+			Console.ForegroundColor = ConsoleColor.DarkCyan;
+			Console.Write(DateTime.Now.ToString("t"));
+			Console.ForegroundColor = color;
+			Console.WriteLine(" " + text);
+			Console.ResetColor();
+		}
+
+		/* store var into file */
+		public static void SerializeObject<T>(T serializableObject, string fileName) { //http://stackoverflow.com/questions/6115721/how-to-save-restore-serializable-object-to-from-file
+			try {
+				XmlDocument xmlDocument = new XmlDocument();
+				XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+				using (MemoryStream stream = new MemoryStream()) {
+					serializer.Serialize(stream, serializableObject);
+					stream.Position = 0;
+					xmlDocument.Load(stream);
+					xmlDocument.Save(fileName);
+					stream.Close();
+				}
+			}
+			catch (Exception ex) {
+				log(ex.ToString(), ConsoleColor.Red);
+			}
+		}
+
+		/* load var from file */
+		public static T DeSerializeObject<T>(string fileName) {
+			T objectOut = default(T);
+			try {
+				XmlDocument xmlDocument = new XmlDocument();
+				xmlDocument.Load(fileName);
+				string xmlString = xmlDocument.OuterXml;
+				using (StringReader read = new StringReader(xmlString)) {
+					Type outType = typeof(T);
+					XmlSerializer serializer = new XmlSerializer(outType);
+					using (XmlReader reader = new XmlTextReader(read)) {
+						objectOut = (T)serializer.Deserialize(reader);
+						reader.Close();
+					}
+					read.Close();
+				}
+			}
+			catch (Exception ex) {
+				log(ex.ToString(), ConsoleColor.Red);
+			}
+			return objectOut;
+		}
+
+		/* human readable time deltas */
+		public static string deltatimeformat(TimeSpan span, string rough = "") {
+			int day		= Convert.ToInt16(span.ToString("%d")),
+				hour	= Convert.ToInt16(span.ToString("%h")),
+				minute	= Convert.ToInt16(span.ToString("%m"));
+
+			if (span.CompareTo(TimeSpan.Zero) == -1) {
+				log("Time to sync the clock?" + span.ToString(), ConsoleColor.Red);
+				return "A few seconds";
+			}
+			else if (day > 1) {
+				if (hour == 0) return day + " days";
+				else return day + " days " + hour + "h";
+			}
+			else if (day == 1) {
+				if (hour == 0) return "a day";
+				else return "a day " + hour + "h";
+			}
+			else { // when day == 0
+				if (hour == 0) return rough + minute + "m";
+				else {
+					if (minute == 0) return rough + hour + "h";
+					else return rough + hour + "h " + minute + "m";
+				}
+			}
 		}
 	}
 }
--- C#/ModClass.cs
+++ C#/ModClass.cs
@@ -7,7 +7,94 @@ using System.Threading.Tasks;
 namespace IRCbot {
 	public static class ModClass{
 		public static void parse(string message) {
-			MainClass.sendmessage("Insiiiiiiiiiide!!!!!!!!!!!!!!");
+
+			/* Simple stuff */
+			if (message.StartsWith("!sing"))	MainClass.sendmessage("/me sings the body electric ♫");
+			if (message.StartsWith("!dance"))	MainClass.sendmessage("/me roboboogies ⌐[º-°⌐] [¬º-°]¬");
+			if (message.StartsWith("!status"))	MainClass.sendmessage("v8.0.0 N:" + MainClass.MyGlobals.ninja + " MA:" + MainClass.MyGlobals.modabuse);
+
+			/* get user's last words */
+			if (message.StartsWith("!stalk ")) {
+				string user = message.Substring(message.IndexOf(" ") + 1).ToLower().Trim();
+				object[] stalkinfo;
+				if (MainClass.MyGlobals.stalk.TryGetValue(user, out stalkinfo)) {
+					string deltatime = MainClass.deltatimeformat(DateTime.Now - (DateTime)stalkinfo[0]);
+					MainClass.sendmessage(user + " seen " + deltatime + " ago saying: " + stalkinfo[1]);
+				}
+				else MainClass.sendmessage("There are no records of " + user);
+			}
+
+			/* In times of spam, the bot doesn't speak. It just bans. */
+			if (message.StartsWith("!ninja ")) {
+				if (message.Contains("on")){
+					MainClass.MyGlobals.ninja = 1;
+					MainClass.sendmessage("I am the blade of Shakuras.");
+				}
+				else if (message.Contains("off")) {
+					MainClass.MyGlobals.ninja = 0;
+					MainClass.sendmessage("The void claims its own.");
+				}
+				MainClass.SerializeObject(MainClass.MyGlobals.ninja, "ninja.xml");
+			}
+
+			/* Banlogic is relaxed during certain periods */
+			if (message.StartsWith("!modabuse ")) {
+				if (message.Contains("on")) {
+					MainClass.MyGlobals.modabuse = 2;
+					MainClass.sendmessage("Justice has come!");
+				}
+				else if (message.Contains("semi")) {
+					MainClass.MyGlobals.modabuse = 1;
+					MainClass.sendmessage("Calibrating void lenses.");
+				}
+				else if (message.Contains("off")) {
+					MainClass.MyGlobals.modabuse = 0;
+					MainClass.sendmessage("Awaiting the call.");
+				}
+				MainClass.SerializeObject(MainClass.MyGlobals.modabuse, "modabuse.xml");
+			}
+			
+			/* Add to autoban list */
+			if (message.StartsWith("!add ")) {
+				string banword = message.Substring(message.IndexOf(" ") + 1).ToLower().Trim();
+				MainClass.MyGlobals.banwords.Add(banword);
+				MainClass.SerializeObject(MainClass.MyGlobals.banwords, "banwords.xml");
+				MainClass.sendmessage(banword + " added to the autoban list.");
+			}
+
+			/* Remove from autoban list */
+			if (message.StartsWith("!del ") || message.StartsWith("!delete ") || message.StartsWith("!remove ")) {
+				string banword = message.Substring(message.IndexOf(" ") + 1).ToLower().Trim();
+				MainClass.MyGlobals.banwords.Remove(banword);
+				if (MainClass.MyGlobals.banwords.Contains(banword)){
+					MainClass.SerializeObject(MainClass.MyGlobals.banwords, "banwords.xml");
+					MainClass.sendmessage(banword + " removed from autoban list.");
+				}
+				else{
+					MainClass.sendmessage(banword + " not in banlist");
+				}
+			}
+
+			/* Add to tempban list */
+			if (message.StartsWith("!tempadd ")) {
+				string banword = message.Substring(message.IndexOf(" ") + 1).ToLower().Trim();
+				MainClass.MyGlobals.tempbanwords.Add(banword);
+				MainClass.SerializeObject(MainClass.MyGlobals.tempbanwords, "tempbanwords.xml");
+				MainClass.sendmessage(banword + " added to the autoban list.");
+			}
+
+			/* Remove from autoban list */
+			if (message.StartsWith("!tempdel ") || message.StartsWith("!dempdelete ") || message.StartsWith("!tempremove ")) {
+				string banword = message.Substring(message.IndexOf(" ") + 1).ToLower().Trim();
+				MainClass.MyGlobals.tempbanwords.Remove(banword);
+				if (MainClass.MyGlobals.tempbanwords.Contains(banword)) {
+					MainClass.SerializeObject(MainClass.MyGlobals.tempbanwords, "tempbanwords.xml");
+					MainClass.sendmessage(banword + " removed from tempban list.");
+				}
+				else {
+					MainClass.sendmessage(banword + " not in tempbanlist");
+				}
+			}
 		}
 	}
 }
